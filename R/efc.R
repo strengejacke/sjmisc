1a079d38f972a5831be3d211aed77bea3d9785a4
Daniel Lüdecke <d.luedecke@uke.de>
2015-06-04 18:54:48 +0200

    removed unnecessary sample datasets

--- R/efc.R
+++ R/efc.R
@@ -8,16 +8,6 @@
 #'
 #' @references \url{http://www.uke.de/eurofamcare/}
 #'
-#' @note There are two further datasets, \code{efc2} and \code{efc3}, which
-#'         slightly differ in their structure.
-#'         \describe{
-#'          \item{efc2}{was read with the \code{\link[foreign]{read.spss}} function
-#'                      and also has attached variable label attributes to each variable.}
-#'          \item{efc3}{categorical variables have been converted to labelled factors,
-#'                      i.e. value labels are set as factor levels. However, factors
-#'                      in \code{efc3} no longer have variable label attributes.}
-#'          }
-#'
 #' @examples
 #' # Attach EFC-data
 #' data(efc)
@@ -41,44 +31,3 @@
 #'
 NULL
 
-
-#' @docType data
-#' @title Sample dataset from the EUROFAMCARE project
-#' @name efc2
-#' @keywords data
-#'
-#' @references \url{http://www.uke.de/eurofamcare/}
-#'
-#' @examples
-#' # Attach EFC-data
-#' data(efc2)
-#'
-#' \dontrun{
-#' # show variables
-#' view_spss(efc2)
-#'
-#' # print frq of dependency
-#' sjt.frq(efc2$e42dep)}
-#'
-NULL
-
-
-#' @docType data
-#' @title Sample dataset from the EUROFAMCARE project
-#' @name efc3
-#' @keywords data
-#'
-#' @references \url{http://www.uke.de/eurofamcare/}
-#'
-#' @examples
-#' # Attach EFC-data
-#' data(efc3)
-#'
-#' str(efc3$e15relat)
-#' table(efc3$e15relat)
-#'
-#' \dontrun{
-#' # print frq of relationships
-#' sjt.frq(efc3$e15relat)}
-#'
-NULL
