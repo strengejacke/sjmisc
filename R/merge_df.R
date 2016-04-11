02f7b42264813db77fecd48875286ae160c5ec92
Daniel Lüdecke <d.luedecke@uke.de>
2016-04-11 17:18:00 +0200

    `merge_df` now optionally merges more than two data frames at once

--- R/merge_df.R
+++ R/merge_df.R
@@ -1,10 +1,11 @@
 #' @title Merge labelled data frames
 #' @name merge_df
 #'
-#' @description Merges (full join) two data frames and preserve value and variable labels.
+#' @description Merges (full join) two (or more) data frames and preserve value and variable labels.
 #'
 #' @param x1 First data frame to be merged.
 #' @param x2 Second data frame to be merged.
+#' @param ... More data frames to be merged.
 #' @param id Optional name for ID column that will be created to indicate the
 #'          source data frames for appended rows.
 #'
@@ -33,8 +34,63 @@
 #' library(sjPlot)
 #' view_df(mydf)}
 #'
+#' x3 <- efc %>% select(5:9) %>% slice(1:10)
+#' x4 <- efc %>% select(11:14) %>% slice(1:10)
+#'
+#' mydf <- merge_df(x1, x2, x3, x4, id = "subsets")
+#' mydf
+#' str(mydf)
+#'
 #' @export
-merge_df <- function(x1, x2, id = NULL) {
+merge_df <- function(x1, x2, ..., id = NULL) {
+  # --------------------------------------------------------
+  # retrieve list of parameters
+  # --------------------------------------------------------
+  more_dfs <- list(...)
+  # --------------------------------------------------------
+  # first step, initial merge
+  # --------------------------------------------------------
+  x_final <- merge_df_helper(x1, x2)
+  # --------------------------------------------------------
+  # merge remaining df's
+  # --------------------------------------------------------
+  # do we have more data frames?
+  if (!is.null(more_dfs) && length(more_dfs) > 0) {
+    # iterate all remaining data frames
+    for (i in 1:length(more_dfs)) {
+      # create ID vector
+      x_final <- merge_df_helper(x_final, more_dfs[[i]])
+    }
+  }
+  # create ID column?
+  if (!is.null(id)) {
+    # check whether column name already exists
+    if (id %in% colnames(x_final)) {
+      warning("Value of `id` already exists as column name. ID column was not created.", call. = F)
+    } else {
+      # create ID vector
+      id_col <- c(rep(deparse(substitute(x1)), times = nrow(x1)),
+                  rep(deparse(substitute(x2)), times = nrow(x2)))
+      # do we have more data frames?
+      if (!is.null(more_dfs) && length(more_dfs) > 0) {
+        # iterate all remaining data frames
+        for (i in 1:length(more_dfs)) {
+          # create ID vector
+          id_col <- c(id_col, rep(sprintf("%s_%i", deparse(substitute(x1)), i),
+                                  times = nrow(more_dfs[[i]])))
+        }
+      }
+      # bind id column
+      x_final <- cbind(x_final, id_col)
+      # name column
+      colnames(x_final)[ncol(x_final)] <- id
+    }
+  }
+  # return merged df
+  x_final
+}
+
+merge_df_helper <- function(x1, x2) {
   # find matching columns in both data frames
   x2_match <- match(colnames(x1), colnames(x2))
   x1_match <- which(!is.na(x2_match))
@@ -51,7 +107,13 @@ merge_df <- function(x1, x2, id = NULL) {
   # of x2 that also appear in x1
   x1_new <- rbind(x1, tmp)
   # which columns are still in x2 and have not been merged yet?
-  x2_remain <- seq_len(ncol(x2))[-x2_match]
+  # in certain cases, e.g. when we have no matching columns at all,
+  # x2_match is of length 0. in this case, all columns are still remaining,
+  # so we need to check this here
+  if (is_empty(x2_match))
+    x2_remain <- seq_len(ncol(x2))
+  else
+    x2_remain <- seq_len(ncol(x2))[-x2_match]
   # create dummy df for remaining data of x2
   tmp <- as.data.frame(matrix(nrow = nrow(x1), ncol = length(x2_remain)))
   colnames(tmp) <- colnames(x2)[x2_remain]
@@ -64,20 +126,6 @@ merge_df <- function(x1, x2, id = NULL) {
   }
   # final merge
   x_final <- cbind(x1_new, tmp)
-  # create ID column?
-  if (!is.null(id)) {
-    # check whether column name already exists
-    if (id %in% colnames(x_final)) {
-      warning("Value of `id` already exists as column name. ID column was not created.", call. = F)
-    } else {
-      # create ID vector
-      id_col <- c(rep(deparse(substitute(x1)), times = nrow(x1)),
-                  rep(deparse(substitute(x2)), times = nrow(x2)))
-      # bind id column
-      x_final <- cbind(x_final, id_col)
-      # name column
-      colnames(x_final)[ncol(x_final)] <- id
-    }
-  }
+  # return merged df
   x_final
 }
