ee6db0fd5c86b1a9235abf9ee0b9edb2e6204679
Daniel Lüdecke <d.luedecke@uke.de>
2016-04-27 18:39:28 +0200

    recode.key in to_long did not sort numeric values

--- R/to_long.R
+++ R/to_long.R
@@ -117,20 +117,18 @@ to_long <- function(data, keys, values, ..., labels = NULL, recode.key = FALSE) 
     # remove those columns that should not be gathered
     tmp <- data[, -match(remove_cols, colnames(data))]
     # gather data frame
-     tmp <- suppressWarnings(
-       tidyr::gather_(tmp,
-                      keys[i],
-                      values[i],
-                      data_cols[[i]])
-       )
-     # need to recode key-value?
-     if (recode.key)
-       tmp[[keys[i]]] <- to_value(tmp[[keys[i]]], keep.labels = FALSE)
-     # set variable label
-     if (!is.null(labels))
-       set_label(tmp[[values[i]]]) <- labels[i]
-     # add output to list
-     dummy[[length(dummy) + 1]] <- tmp
+    tmp <- suppressWarnings(tidyr::gather_(tmp,
+                                           keys[i],
+                                           values[i],
+                                           data_cols[[i]]))
+    # need to recode key-value?
+    if (recode.key)
+      tmp[[keys[i]]] <- sort(to_value(tmp[[keys[i]]], keep.labels = FALSE))
+    # set variable label
+    if (!is.null(labels))
+      set_label(tmp[[values[i]]]) <- labels[i]
+    # add output to list
+    dummy[[length(dummy) + 1]] <- tmp
   }
   # we have at least one gathered data frame
   mydat <- dummy[[1]]
