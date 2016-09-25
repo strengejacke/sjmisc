979f012963d7b05d37e0732b3fbdca4e7045f8eb
Daniel Lüdecke <d.luedecke@uke.de>
2016-09-25 14:29:12 +0200

    Create is_labelled.R

---
+++ R/is_labelled.R
@@ -0,0 +1,11 @@
+
+#' @title Check whether object is of class "labelled"
+#' @name is_labelled
+#' @description This function checks whether \code{x} is of class \code{labelled}.
+#'
+#' @param x An object.
+#' @return Logical, \code{TRUE} if \code{any(class(x))} is \code{labelled},
+#'           \code{FALSE} otherwise.
+#'
+#' @export
+is_labelled <- function(x) inherits(x, "labelled")
