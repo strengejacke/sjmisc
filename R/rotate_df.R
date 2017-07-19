8426187072d96f9edc7fa0ba7a5d39ccdbdd5305
Daniel <mail@danielluedecke.de>
2017-07-19 21:06:57 +0200

    `rotate_df()` gets option to use 1st col. values as column names

--- R/rotate_df.R
+++ R/rotate_df.R
@@ -7,6 +7,8 @@
 #' @param rn Character vector (optional). If not \code{NULL}, the data frame's
 #'           rownames will be added as (first) column to the output, with
 #'           \code{rn} being the name of this column.
+#' @param cn Logical (optional), if \code{TRUE}, the values of the first column
+#'           in \code{x} will be used as column names in the rotated data frame.
 #'
 #' @return A (rotated) data frame.
 #'
@@ -14,6 +16,13 @@
 #' x <- mtcars[1:3, 1:4]
 #' rotate_df(x)
 #' rotate_df(x, rn = "property")
+#'
+#' # use values in 1. column as column name
+#' library(tibble)
+#' x <- tibble::rownames_to_column(x)
+#' rotate_df(x)
+#' rotate_df(x, cn = TRUE)
+#' rotate_df(x, rn = "property", cn = TRUE)
 #'
 #' # also works on list-results
 #' library(purrr)
@@ -39,13 +48,34 @@
 #' rotate_df(tmp)
 #'
 #' @importFrom tibble rownames_to_column
+#' @importFrom dplyr select
 #' @export
-rotate_df <- function(x, rn = NULL) {
+rotate_df <- function(x, rn = NULL, cn = FALSE) {
+  # check if first column has column names
+  # that should be used for rotated df
+  if (cn) {
+    cnames <- x[[1]]
+    x <- dplyr::select(x, -1)
+  } else
+    cnames <- NULL
+
+
   # rotate data frame for 90°
   x <- x %>%
     as.data.frame() %>%
     t() %>%
     as.data.frame()
+
+
+  # add column names, if requested
+  if (!is.null(cnames)) {
+    # check if we have correct length of column names
+    if (length(cnames) != ncol(x))
+      warning("Length of provided column names does not match number of columns. No column names changed.", call. = FALSE)
+    else
+      colnames(x) <- cnames
+  }
+
 
   # add rownames as column, if requested
   if (!is.null(rn)) x <- tibble::rownames_to_column(x, var = rn)
