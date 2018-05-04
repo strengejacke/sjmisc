3ab0618f11cbcedd1eed7fd7786f1294ec24525c
Daniel <mail@danielluedecke.de>
2018-05-04 19:02:16 +0200

    release v2.7.2 on CRAN, at May The Fourth!

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,7 @@
     packageStartupMessage("Learn more about sjmisc with 'browseVignettes(\"sjmisc\")'.")
   } else if (stats::runif(1) > .9) {
     packageStartupMessage("Install package \"strengejacke\" from GitHub (`devtools::install_github(\"strengejacke/strengejacke\")`) to load all sj-packages at once!")
+  } else if (format(Sys.time(), "%m%d") == "0504") {
+    packageStartupMessage("May the fourth be with you!")
   }
 }
