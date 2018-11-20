3e9ce90b6ad894a65f364fd406a364c912cd9435
Daniel <mail@danielluedecke.de>
2018-11-20 19:04:45 +0100

    update test

--- tests/testthat/test-to_dummy.R
+++ tests/testthat/test-to_dummy.R
@@ -1,9 +1,9 @@
-context("sjmisc, count_na")
+context("sjmisc, to_dummy")
 
 library(sjmisc)
 
-x <- to_dummy(data.frame(x = c("yes", "no", "yes", "maybe"), stringsAsFactors = F), suffix = "label")
-y <- to_dummy(data.frame(x = c("yes", "no", "yes", "maybe")), suffix = "label")
+x <- to_dummy(data.frame(x = c("yes", "no", "yes", "maybe"), stringsAsFactors = FALSE), suffix = "label")
+y <- to_dummy(data.frame(x = c("yes", "no", "yes", "maybe"), stringsAsFactors = TRUE), suffix = "label")
 
 test_that("to_dummy", {
   expect_identical(x, y)
